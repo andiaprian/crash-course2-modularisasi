802d8117b2135db69650fafef9eebbace6daaf00
andiapriana <aaprianadewi@gmail.com>
2020-12-17 21:18:55 +0700

    modularisasi_tahap_1

---
+++ modularisasi_tahap_1.py
@@ -0,0 +1,24 @@
+"""
+Program menghitung luas segitiga
+luas_segitiga = alas * tinggi / 2
+"""
+
+alas = 20
+tinggi = 2
+luas_segitiga = alas * tinggi / 2
+print(f'Segitiga dengan alas={alas} dan tinggi={tinggi} memiliki luas {luas_segitiga}')
+
+print('Mengitung luas segitiga dengan copy paste')
+alas = 20
+tinggi = 2
+luas_segitiga = alas * tinggi / 2
+print(f'Segitiga dengan alas={alas} dan tinggi={tinggi} memiliki luas {luas_segitiga}')
+
+print('\nMembuat fungsi hitung_luas_segitiga')
+def hitung_luas_segitiga(alas, tinggi):
+    luas_segitiga = alas * tinggi / 2K
+    return luas_segitiga
+
+print(f'Menghitung segitiga dengan fungsi, hasilnya = {hitung_luas_segitiga(10, 6)}')
+print(f'Menghitung segitiga dengan fungsi, hasilnya = {hitung_luas_segitiga(20, 2)}')
+print(f'Menghitung segitiga dengan fungsi, hasilnya = {hitung_luas_segitiga(100, 2)}')
